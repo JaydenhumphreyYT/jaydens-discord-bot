5a0d347ebbf3bfd2f5544effb31f4ed020a60305
JaydenhumphreyYT <42541462+JaydenhumphreyYT@users.noreply.github.com>
2018-11-26 06:15:45 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-bot.login(process.env.NTE2NTQ2MzQ0MzY2NjM3MDc4.Dt2PRA.wXMnkQnDuZ-q8qzhfufFsnfC-uQ);//where BOT_TOKEN is the token of our bot 
+bot.login("NTE2NTQ2MzQ0MzY2NjM3MDc4.Dt2PRA.wXMnkQnDuZ-q8qzhfufFsnfC-uQ");//where BOT_TOKEN is the token of our bot 
